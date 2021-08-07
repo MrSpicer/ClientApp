d88f17f6cfcfd4bc67261c312f27f771ad31acf8
Justin <justin@justinspicer.com>
2021-08-07 14:04:58 -0500

    bulma wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bulma'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section class="hero is-medium is-primary">
+        <div class="hero-body">
+          <div class="container">
+            <div class="columns">
+              <div class="column is-8-desktop is-offset-2-desktop">
+                <h1 class="title is-2 is-spaced">
+                  Bulma
+              </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="section">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-8-desktop is-offset-2-desktop">
+              <div class="content">
+                Content goes here
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer class="footer has-text-centered">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-8-desktop is-offset-2-desktop">
+              Feet
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
 
 export default App;
+
+
